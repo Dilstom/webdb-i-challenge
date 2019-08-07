ed393dfec0eca94cc2abf9076b5edcbfc8586d3e
Dil Sombayeva <dilyasom@gmail.com>
2019-08-06 23:25:45 -0700

    and delete req

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -58,4 +58,22 @@ router.put('/:id', async (req, res) => {
  }
 });
 
+router.delete('/:id', async (req, res) => {
+ const { id } = req.params;
+ try {
+  const count = await db('accounts')
+   .where({ id })
+   .del();
+  if (count) {
+   res.status(200).json({ message: 'this accound has been deleted', count });
+  } else {
+   res.status(404).json({ message: 'could not find this id' });
+  }
+ } catch (err) {
+  res
+   .status(500)
+   .json({ message: 'could not delete this account', error: err });
+ }
+});
+
 module.exports = router;
